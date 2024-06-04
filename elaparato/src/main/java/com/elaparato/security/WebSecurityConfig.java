26c75e774165acc9d5fbcfad875a96b4fee71c1e
melissabonillasellanes <melibonilla3@gmail.com>
2024-06-03 22:30:38 -0300

    Me acabo de dar cuenta que no había quedado subida la carpeta de security del paquete elaparato

--- elaparato/src/main/java/com/elaparato/security/WebSecurityConfig.java
+++ elaparato/src/main/java/com/elaparato/security/WebSecurityConfig.java
@@ -1,2 +1,37 @@
-package com.elaparato.security;public class WebSecurityConfig {
+package com.elaparato.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+    public static final String ADMIN = "app_administrador";
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/users/**").hasAnyRole(ADMIN)
+
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())
+                        )
+                );
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("http://localhost:8080/realms/el-aparato-melissa-bonilla/protocol/openid-connect/certs").build();
+    }
 }
